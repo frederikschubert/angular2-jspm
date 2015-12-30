fd98f29cb7afd6f83b256e369091e9175b69b7cb
Frederik Schubert <frederik.schubert@inside-m2m.de>
2015-12-30 15:47:01 +0100

    Added hot reloading for the root styles in src/style.scss

--- gulp/compile.js
+++ gulp/compile.js
@@ -6,7 +6,7 @@ var autoprefixer = require('gulp-autoprefixer'),
     cache = require('gulp-cached');
 
 gulp.task('compile:css', function () {
-    return gulp.src(global.paths.src + global.paths.scss)
+    gulp.src([global.paths.src + global.paths.scss, '!src/style.scss'])
         .pipe(cache('css'))
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
@@ -14,23 +14,31 @@ gulp.task('compile:css', function () {
         .on('error', function (error) {
             console.error('css error: ' + error);
         });
+    gulp.src(global.paths.src + '/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+        .pipe(gulp.dest(global.paths.www))
+        .pipe(connect.reload())
+        .on('error', function (error) {
+            console.error('css error: ' + error);
+        });
 });
 
 gulp.task('compile:html', function () {
-    return gulp.src(global.paths.src + global.paths.html)
+    gulp.src([global.paths.src + global.paths.html, '!' + global.paths.src + '/index.prod.html'])
         .pipe(cache('html'))
         .pipe(gulp.dest(global.paths.www));
 });
 
 gulp.task('compile:img', function () {
-    return gulp.src(global.paths.src + global.paths.img)
+    gulp.src(global.paths.src + global.paths.img)
         .pipe(cache('img'))
         .pipe(gulp.dest(global.paths.www))
         .pipe(connect.reload());
 });
 
 gulp.task('compile:ts', function () {
-    return gulp.src(global.paths.src + global.paths.ts)
+    gulp.src([global.paths.src + global.paths.ts, '!src/bootstrap.prod.ts'])
         .pipe(cache('ts'))
         .pipe(gulp.dest(global.paths.www));
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,17 +2,13 @@ import {Component} from "angular2/core";
 import {ShowCase} from "./components/showCase/showCase";
 
 @Component({
-	selector: "app",
-	directives: [ShowCase],
-	template: `
-	<h1>Angular2 - JSPM</h1>
-	<showcase></showcase>
-	`
+    selector: "app",
+    directives: [ShowCase],
+    template: `
+    <h1>Angular2 - JSPM</h1>
+    <showcase></showcase>
+    `
 })
 export class App {
-
-    ngOnInit() {
-        console.log("App");
-    }
 
 }
